65d0b8927699ab99fb9f872d339e050d48b77d95
Takiul Hasan <sakibh022@gmail.com>
2021-09-24 21:05:02 +0600

    first

---
+++ TadaManagement/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TadaManagement.Models;
+
+namespace TadaManagement.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        TadaManageEntities db = new TadaManageEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user obja)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (TadaManageEntities db=new TadaManageEntities())
+                {
+
+                    var obj = db.user.Where(a => a.username.Equals(obja.username) && a.password.Equals(obja.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserId"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The UserName or Password Incorrect");
+                    }
+                }
+            }
+            
+            return View(obja);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
---
+++ TadaManagement/Controllers/registrationsController.cs
@@ -0,0 +1,181 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using TadaManagement.Models;
+
+namespace TadaManagement.Controllers
+{
+    public class registrationsController : Controller
+    {
+        private TadaManageEntities db = new TadaManageEntities();
+
+        // GET: registrations
+        public ActionResult Index()
+        {
+            var registration = db.registration.Include(r => r.batch).Include(r => r.course);
+            return View(registration.ToList());
+        }
+        public ActionResult ExportEmployee()
+        {
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Report"), "CrystalReport1.rpt"));
+            rd.SetDataSource(ListToDataTable(db.registration.ToList()));
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "TADA_HISTORY.pdf");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private DataTable ListToDataTable<T>(List<T> items)
+        {
+
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    values[i] = Props[i].GetValue(item, null);
+
+                }
+                dataTable.Rows.Add(values);
+
+            }
+            return dataTable;
+        }
+
+        // GET: registrations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: registrations/Create
+        public ActionResult Create()
+        {
+            ViewBag.batch_id = new SelectList(db.batch, "id", "batch1");
+            ViewBag.course_id = new SelectList(db.course, "id", "course1");
+            return View();
+        }
+
+        // POST: registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id,telno")] registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registration.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.batch_id = new SelectList(db.batch, "id", "batch1", registration.batch_id);
+            ViewBag.course_id = new SelectList(db.course, "id", "course1", registration.course_id);
+            return View(registration);
+        }
+
+        // GET: registrations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.batch_id = new SelectList(db.batch, "id", "batch1", registration.batch_id);
+            ViewBag.course_id = new SelectList(db.course, "id", "course1", registration.course_id);
+            return View(registration);
+        }
+
+        // POST: registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id,telno")] registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.batch_id = new SelectList(db.batch, "id", "batch1", registration.batch_id);
+            ViewBag.course_id = new SelectList(db.course, "id", "course1", registration.course_id);
+            return View(registration);
+        }
+
+        // GET: registrations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registration registration = db.registration.Find(id);
+            db.registration.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
